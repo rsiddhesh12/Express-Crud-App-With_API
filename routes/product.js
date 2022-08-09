9a472b01716acf122d6decd36ea7a60638f58de7
Siddhesh Rane <rsiddhesh12@gmail.com>
2022-08-09 17:09:03 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -10,8 +10,8 @@ var uuid = require("uuid");
 
 
 AWS.config.update({
-  accessKeyId: "AKIAZX5IKU6G4CRP6SSC",
-  secretAccessKey: "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",
+  accessKeyId: "**",
+  secretAccessKey: "************",
   // "region": "sa-east-1" 
 }); // for simplicity. In prod, use loadConfigFromFile, or env variables
 
